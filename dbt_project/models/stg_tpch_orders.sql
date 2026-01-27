05099d73a0da10b8664f2a8230704a8f0867f385
Christopher González Delgado <christopher.gonzalez@plainconcepts.com>
2026-01-27 22:16:18 +0100

    commit

---
+++ dbt_project/models/stg_tpch_orders.sql
@@ -0,0 +1,15 @@
+with cte_orders as (
+    select 
+        o_orderkey as order_key,
+        o_custkey as customer_key,
+        o_orderstatus as status_code,
+        o_totalprice as total_price,
+        o_orderdate as order_date
+    from
+        {{ source('tpch', 'orders')}}
+)
+
+select 
+    *
+from 
+    cte_orders
